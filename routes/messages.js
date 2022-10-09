b0ece92f69eb18ed3a7ab601af750162690f0624
Diana Itzel de la O Veracruz <l19350640@tuxtepec.tecnm.mx>
2022-10-09 13:57:08 -0500

    backendB-4

--- routes/messages.js
+++ routes/messages.js
@@ -2,12 +2,18 @@
 
 const {Router} = require('express')
 const router = Router()
-const {rootMessage, hiMessage, byeMessage} = require('../controllers/messages')
+const {rootMessage, hiMessage, byeMessage, postMessage, putMessage, deleteMessage} = require('../controllers/messages')
 
-    router.get('/', rootMessage)//End Point
+    router.get('/', rootMessage)//End Point//consultas
 
     router.get('/hi', hiMessage) //End Point
 
     router.get('/bye', byeMessage) //End Point
+
+    router.post('/', postMessage) //crear o añadir registros
+
+    router.put('/', putMessage) //actualizar registros
+
+    router.delete('/', deleteMessage) //eliminar registros
 
     module.exports = router
