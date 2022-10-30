0146642329ed4b105d82df9b0795034ca5225a8c
Diana Itzel de la O Veracruz <l19350640@tuxtepec.tecnm.mx>
2022-10-30 12:44:32 -0600

    backendB-12
    	modificados:     controllers/usuarios.js
    	modificados:     routes/usuarios.js

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -14,6 +15,11 @@ const getUsers = (req = request, res = response) => {
 const {request, response } = require("express")
 const pool = require("../db/connection");
 
+=======
+const {request, response } = require("express")
+const pool = require("../db/connection");
+//htpp://localhost:4000/api/v1/usuarios
+>>>>>>> 6f792d8 (backendB-12)
 const getUsers = async (req = request, res = response) => {
    let conn;
 
@@ -23,6 +29,7 @@ const getUsers = async (req = request, res = response) => {
        //Generamos la consulta
        const users = await conn.query("SELECT * FROM Usuarios", (error) => { if (error) throw error })
 
+<<<<<<< HEAD
        if (!users) {//en caso de no haber registros lo informamos
           res.status(404).json({msg: "No existen usuarios registrados"})
           return
@@ -47,6 +54,29 @@ module.exports = {getUsers}
 
 }
 
+=======
+       if (!users.length===0) {//en caso de no haber registros lo informamos
+          res.status(404).json({msg: "No existen usuarios registrados"})
+          return
+       }
+       
+       res.json({users})//se manda la lista de usuarios
+
+   } 
+   
+   catch (error) {
+   console.log(error)
+   res.status(500).json({msg: error})//informamos el error
+   } 
+   finally {
+       if (conn) conn.end()//Termina la conexion
+   }
+
+}
+   //console.log("Funcion getUsers")
+   //res.json(msg: "Funcion getUsers"})
+//}
+>>>>>>> 6f792d8 (backendB-12)
 const getUserByID = async (req = request, res = response) =>{
     const {id} = req.params
     let conn;
@@ -58,10 +88,13 @@ const getUserByID = async (req = request, res = response) =>{
        const users = await conn.query(`SELECT * FROM Usuarios WHERE ID = ${id}`, (error) => { if (error) throw error })
 
 <<<<<<< HEAD
+<<<<<<< HEAD
        console.log(users)
 
 =======
 >>>>>>> a2c19e0350d734b9bbe2769acbd7b120647f9379
+=======
+>>>>>>> 6f792d8 (backendB-12)
        if (!users) {//en caso de no haber registros lo informamos
           res.status(404).json({msg: `No existen usuarios registrados con el ID ${id}`})
           return
@@ -77,7 +110,11 @@ const getUserByID = async (req = request, res = response) =>{
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const deleteUserByID = async (req = request, res = response) =>{
+=======
+const deleteUsersByID = async (req = request, res = response) =>{
+>>>>>>> 6f792d8 (backendB-12)
     const {id} = req.params
     let conn;
 
@@ -87,8 +124,11 @@ const deleteUserByID = async (req = request, res = response) =>{
        //Generamos la consulta
        const result = await conn.query(`UPDATE Usuarios SET Activo = 'N' WHERE ID = ${id}`, (error) => { if (error) throw error })
 
+<<<<<<< HEAD
        console.log(result.affectedRows)
 
+=======
+>>>>>>> 6f792d8 (backendB-12)
        if (result.affectedRows == 0) {//en caso de no haber registros lo informamos
           res.status(404).json({msg: `No existen usuarios registrados con el ID ${id}`})
           return
@@ -102,8 +142,80 @@ const deleteUserByID = async (req = request, res = response) =>{
        if (conn) conn.end()//Termina la conexion
    }
 }
+<<<<<<< HEAD
 module.exports = {getUsers, getUserByID, deleteUserByID}
 =======
 module.exports = {getUsers, getUserByID}
 >>>>>>> ed1118d (backendB-10)
 >>>>>>> a2c19e0350d734b9bbe2769acbd7b120647f9379
+=======
+
+const addUser = async (req = request, res = response) =>{
+    const {Nombre,
+           Apellidos,
+           Edad,
+           Genero,
+           Usuario,
+           Contraseña,
+           Fecha_Nacimiento,
+           Activo}= req.body//URI Params
+
+    if(!Nombre||
+       !Apellidos||
+       !Edad||
+       !Genero||
+       !Usuario||
+       !Contraseña||
+       !Fecha_Nacimiento||
+       !Activo) 
+    {
+        res.status(400).json({msg: "Faltan datos"})
+        return
+    }
+
+    let conn;
+
+   try {
+       conn = await pool.getConnection()//realizamos la conexion
+
+
+       //Generamos la consulta
+       const result = await conn.query(`
+       INSERT INTO Usuarios(
+        Nombre,
+        Apellidos,
+        Edad,
+        Genero,
+        Usuario,
+        Contraseña,
+        Fecha_Nacimiento,
+        Activo)
+       VALUES(
+        '${Nombre}',
+        '${Apellidos}',
+        '${Edad}',
+        '${Genero}',
+        '${Usuario}',
+        '${Contraseña}',
+        '${Fecha_Nacimiento}',
+        '${Activo}'
+       )
+        `, (error) => { if (error) throw error })
+
+       if (result.affectedRows == 0) {//en caso de no haber registros lo informamos
+          res.status(404).json({msg: `No se pudo agregar el usuario`})
+          return
+       }
+
+       res.json({msg: `Se agrega satisfactoriamente el usuario`})//se manda la lista de usuarios
+       }
+       catch (error) {
+           console.log(error)
+           res.status(500).json({msg: error})//informamos el error
+        } 
+        finally{
+            if (conn) conn.end()//Termina la conexion
+   }
+}
+module.exports = {getUsers, getUserByID, addUser, deleteUsersByID}
+>>>>>>> 6f792d8 (backendB-12)
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,6 @@
 const {Router} = require("express")
 <<<<<<< HEAD
+<<<<<<< HEAD
 const {getUsers, getUserByID, deleteUserByID} = require("../controllers/usuarios")
 =======
 <<<<<<< HEAD
@@ -12,15 +13,22 @@ router.get("/", getUsers)
 =======
 const {getUsers, getUserByID} = require("../controllers/usuarios")
 >>>>>>> a2c19e0350d734b9bbe2769acbd7b120647f9379
+=======
+const {getUsers, getUserByID, deleteUsersByID, addUser} = require("../controllers/usuarios")
+>>>>>>> 6f792d8 (backendB-12)
 const router = Router()
 
 //http://localhost:4000/api/v1/usuarios/id
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6f792d8 (backendB-12)
 ///GET///
 router.get("/", getUsers)
 router.get("/id/:id", getUserByID)
 
+<<<<<<< HEAD
 ///DELETE///
 router.delete("/id/:id", deleteUserByID)
 =======
@@ -28,5 +36,12 @@ router.get("/", getUsers)
 router.get("/id/:id", getUserByID)
 >>>>>>> ed1118d (backendB-10)
 >>>>>>> a2c19e0350d734b9bbe2769acbd7b120647f9379
+=======
+///POST///
+router.post("/", addUser)
+
+///DELETE///
+router.delete("/id/:id", deleteUsersByID)
+>>>>>>> 6f792d8 (backendB-12)
 
 module.exports = router
