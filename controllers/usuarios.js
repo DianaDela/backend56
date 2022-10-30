a2c19e0350d734b9bbe2769acbd7b120647f9379
Diana Itzel de la O Veracruz <l19350640@tuxtepec.tecnm.mx>
2022-10-30 12:11:45 -0600

    backendB-10
    	modificados:     controllers/usuarios.js
    	modificados:     routes/usuarios.js

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,10 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 const {request, resonse } = require("express")
 
 const getUsers = (req = request, res = response) => {
     console.log("Funcion getUsers")
     res.json({msg: "Funcion getUsers"})
 =======
+=======
+>>>>>>> ed1118d (backendB-10)
 const {request, response } = require("express")
 const pool = require("../db/connection");
 
@@ -29,7 +32,38 @@ const getUsers = async (req = request, res = response) => {
    } finally {
        if (conn) conn.end()//Termina la conexion
    }
+<<<<<<< HEAD
 >>>>>>> cdec3f3 (backendB-9)
 }
 
 module.exports = {getUsers}
+=======
+
+}
+
+const getUserByID = async (req = request, res = response) =>{
+    const {id} = req.params
+    let conn;
+
+   try {
+       conn = await pool.getConnection()//realizamos la conexion
+
+       //Generamos la consulta
+       const users = await conn.query(`SELECT * FROM Usuarios WHERE ID = ${id}`, (error) => { if (error) throw error })
+
+       if (!users) {//en caso de no haber registros lo informamos
+          res.status(404).json({msg: `No existen usuarios registrados con el ID ${id}`})
+          return
+       }
+
+       res.json({users})//se manda la lista de usuarios
+   }catch (error) {
+       console.log(error)
+       res.status(500).json({msg: error})//informamos el error
+   } finally {
+       if (conn) conn.end()//Termina la conexion
+   }
+}
+
+module.exports = {getUsers, getUserByID}
+>>>>>>> ed1118d (backendB-10)
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,19 @@
 const {Router} = require("express")
+<<<<<<< HEAD
 const {getUsers} = require("../controllers/usuarios")
 const router = Router()
 
 //http://localhost:4000/api/v1/users/
 
 router.get("/", getUsers)
+=======
+const {getUsers, getUserByID} = require("../controllers/usuarios")
+const router = Router()
+
+//http://localhost:4000/api/v1/usuarios/id
+
+router.get("/", getUsers)
+router.get("/id/:id", getUserByID)
+>>>>>>> ed1118d (backendB-10)
 
 module.exports = router
