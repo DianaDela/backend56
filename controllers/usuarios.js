75967b3d1d91fffdc829fc8fbb26f79498255766
Diana Itzel de la O Veracruz <l19350640@tuxtepec.tecnm.mx>
2022-10-30 13:00:40 -0600

    backendB-14
    	modificados:     controllers/usuarios.js
    	modificados:     routes/usuarios.js

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -26,6 +27,11 @@ const {request, response } = require("express")
 const pool = require("../db/connection");
 //htpp://localhost:4000/api/v1/usuarios
 >>>>>>> d1eb93c (backendB-13)
+=======
+const {request, response } = require("express")
+const pool = require("../db/connection");
+//htpp://localhost:4000/api/v1/usuarios
+>>>>>>> fa65de0 (backendB-14)
 const getUsers = async (req = request, res = response) => {
    let conn;
 
@@ -35,6 +41,7 @@ const getUsers = async (req = request, res = response) => {
        //Generamos la consulta
        const users = await conn.query("SELECT * FROM Usuarios", (error) => { if (error) throw error })
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
        if (!users) {//en caso de no haber registros lo informamos
@@ -64,6 +71,8 @@ module.exports = {getUsers}
 =======
 =======
 >>>>>>> d1eb93c (backendB-13)
+=======
+>>>>>>> fa65de0 (backendB-14)
        if (!users.length===0) {//en caso de no haber registros lo informamos
           res.status(404).json({msg: "No existen usuarios registrados"})
           return
@@ -86,9 +95,12 @@ module.exports = {getUsers}
    //res.json(msg: "Funcion getUsers"})
 //}
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 6f792d8 (backendB-12)
 =======
 >>>>>>> d1eb93c (backendB-13)
+=======
+>>>>>>> fa65de0 (backendB-14)
 const getUserByID = async (req = request, res = response) =>{
     const {id} = req.params
     let conn;
@@ -102,6 +114,7 @@ const getUserByID = async (req = request, res = response) =>{
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
        console.log(users)
 
 =======
@@ -110,6 +123,8 @@ const getUserByID = async (req = request, res = response) =>{
 >>>>>>> 6f792d8 (backendB-12)
 =======
 >>>>>>> d1eb93c (backendB-13)
+=======
+>>>>>>> fa65de0 (backendB-14)
        if (!users) {//en caso de no haber registros lo informamos
           res.status(404).json({msg: `No existen usuarios registrados con el ID ${id}`})
           return
@@ -127,6 +142,7 @@ const getUserByID = async (req = request, res = response) =>{
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const deleteUserByID = async (req = request, res = response) =>{
 =======
 const deleteUsersByID = async (req = request, res = response) =>{
@@ -134,6 +150,9 @@ const deleteUsersByID = async (req = request, res = response) =>{
 =======
 const deleteUsersByID = async (req = request, res = response) =>{
 >>>>>>> d1eb93c (backendB-13)
+=======
+const deleteUsersByID = async (req = request, res = response) =>{
+>>>>>>> fa65de0 (backendB-14)
     const {id} = req.params
     let conn;
 
@@ -145,12 +164,15 @@ const deleteUsersByID = async (req = request, res = response) =>{
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
        console.log(result.affectedRows)
 
 =======
 >>>>>>> 6f792d8 (backendB-12)
 =======
 >>>>>>> d1eb93c (backendB-13)
+=======
+>>>>>>> fa65de0 (backendB-14)
        if (result.affectedRows == 0) {//en caso de no haber registros lo informamos
           res.status(404).json({msg: `No existen usuarios registrados con el ID ${id}`})
           return
@@ -164,6 +186,7 @@ const deleteUsersByID = async (req = request, res = response) =>{
        if (conn) conn.end()//Termina la conexion
    }
 }
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 module.exports = {getUsers, getUserByID, deleteUserByID}
@@ -184,6 +207,19 @@ const addUser = async (req = request, res = response) =>{
            Contraseña,
            Fecha_Nacimiento,
            Activo}= req.body//URI Params
+=======
+
+const addUser= async (req = request, res = response) =>{
+    const {
+        Nombre,
+        Apellidos,
+        Edad,
+        Genero,
+        Usuario,
+        Contraseña,
+        Fecha_Nacimiento,
+        Activo}= req.body//URI Params
+>>>>>>> fa65de0 (backendB-14)
 
     if(!Nombre||
        !Apellidos||
@@ -191,8 +227,12 @@ const addUser = async (req = request, res = response) =>{
        !Genero||
        !Usuario||
        !Contraseña||
+<<<<<<< HEAD
        !Fecha_Nacimiento||
        !Activo) 
+=======
+       !Activo)
+>>>>>>> fa65de0 (backendB-14)
     {
         res.status(400).json({msg: "Faltan datos"})
         return
@@ -200,18 +240,29 @@ const addUser = async (req = request, res = response) =>{
 
     let conn;
 
+<<<<<<< HEAD
    try {
        conn = await pool.getConnection()//realizamos la conexion
 
 <<<<<<< HEAD
 =======
+=======
+    //validar que no existe el usuario
+
+   try {
+       conn = await pool.getConnection()//realizamos la conexion
+
+>>>>>>> fa65de0 (backendB-14)
        const [userExist] = await conn.query(`SELECT Usuario From Usuarios WHERE Usuario = '${Usuario}'`)
 
        if (userExist) {
         res.status(400).json({msg: `El usuario ${Usuario} ya se encuentra registrado.`})
         return
        }
+<<<<<<< HEAD
 >>>>>>> d1eb93c (backendB-13)
+=======
+>>>>>>> fa65de0 (backendB-14)
 
        //Generamos la consulta
        const result = await conn.query(`
@@ -224,7 +275,11 @@ const addUser = async (req = request, res = response) =>{
         Contraseña,
         Fecha_Nacimiento,
         Activo)
+<<<<<<< HEAD
        VALUES(
+=======
+       VALUE(
+>>>>>>> fa65de0 (backendB-14)
         '${Nombre}',
         '${Apellidos}',
         '${Edad}',
@@ -252,8 +307,75 @@ const addUser = async (req = request, res = response) =>{
    }
 }
 <<<<<<< HEAD
+<<<<<<< HEAD
 module.exports = {getUsers, getUserByID, addUser, deleteUsersByID}
 >>>>>>> 6f792d8 (backendB-12)
 =======
 module.exports = {getUsers, getUserByID, addUser, deleteUsersByID}
 >>>>>>> d1eb93c (backendB-13)
+=======
+
+const updateUserByUsuario= async (req = request, res = response) =>{
+    const {
+        Nombre,
+        Apellidos,
+        Edad,
+        Genero,
+        Usuario,
+        Fecha_Nacimiento = '1900-01-01',
+        }= req.body//URI Params
+
+    if(!Nombre||
+       !Apellidos||
+       !Edad||
+       !Usuario
+    ){
+        res.status(400).json({msg: "Faltan datos"})
+        return
+    }
+
+    let conn;
+
+    //validar que no existe el usuario
+
+   try {
+       conn = await pool.getConnection()//realizamos la conexion
+
+       const [userExist] = await conn.query(`SELECT Usuario From Usuarios WHERE Usuario = '${Usuario}'`)
+
+       if (userExist) {
+        res.status(400).json({msg: `El usuario ${Usuario} no se encuentra registrado.`})
+        return
+       }
+
+       //Generamos la consulta
+       const result = await conn.query(`
+       UPDATE Usuarios SET
+        Nombre = '${Nombre}',
+        Apellidos ='${Apellidos}',
+        Edad ='${Edad}',
+        ${Genero ? `Genero ='${Genero}',` : ''}
+        Fecha_Nacimiento ='${Fecha_Nacimiento}',
+       WHERE
+           Usuario = '${Usuario}'
+        `, (error) => { if (error) throw error })
+
+       if (result.affectedRows == 0) {//en caso de no haber registros lo informamos
+          res.status(404).json({msg: `No se pudo agregar el usuario`})
+          return
+       }
+
+       res.json({msg: `Se agrega satisfactoriamente el usuario`})//se manda la lista de usuarios
+       }
+       catch (error) {
+           console.log(error)
+           res.status(500).json({msg: error})//informamos el error
+        } 
+        finally{
+            if (conn) conn.end()//Termina la conexion
+   }
+}
+
+
+module.exports = {getUsers, getUserByID, addUser,  deleteUsersByID, updateUserByUsuario}
+>>>>>>> fa65de0 (backendB-14)
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,6 +2,7 @@ const {Router} = require("express")
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const {getUsers, getUserByID, deleteUserByID} = require("../controllers/usuarios")
 =======
 <<<<<<< HEAD
@@ -20,6 +21,9 @@ const {getUsers, getUserByID, deleteUsersByID, addUser} = require("../controller
 =======
 const {getUsers, getUserByID, deleteUsersByID, addUser} = require("../controllers/usuarios")
 >>>>>>> d1eb93c (backendB-13)
+=======
+const {getUsers, getUserByID, deleteUsersByID, addUser, updateUserByUsuario} = require("../controllers/usuarios")
+>>>>>>> fa65de0 (backendB-14)
 const router = Router()
 
 //http://localhost:4000/api/v1/usuarios/id
@@ -27,14 +31,18 @@ const router = Router()
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 6f792d8 (backendB-12)
 =======
 >>>>>>> d1eb93c (backendB-13)
+=======
+>>>>>>> fa65de0 (backendB-14)
 ///GET///
 router.get("/", getUsers)
 router.get("/id/:id", getUserByID)
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 ///DELETE///
@@ -56,5 +64,15 @@ router.delete("/id/:id", deleteUsersByID)
 >>>>>>> 6f792d8 (backendB-12)
 =======
 >>>>>>> d1eb93c (backendB-13)
+=======
+///POST///
+router.post("/", addUser)
+
+///PUT///
+router.put("/", updateUserByUsuario)
+
+///DELETE///
+router.delete("/id/:id", deleteUsersByID)
+>>>>>>> fa65de0 (backendB-14)
 
 module.exports = router
